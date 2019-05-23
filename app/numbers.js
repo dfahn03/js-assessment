7c17d9f31f8967e090de99f59b2b1f96ad78cdad
Dallin Fahnestock <dfahn03@gmail.com>
2019-05-23 15:04:42 -0600

    arrays & numbers done

--- app/numbers.js
+++ app/numbers.js
@@ -12,7 +12,11 @@ let sum = one + two + three;
 // 2. write a loop below that starts at 20 and ends at 100. Check each number,
 // if the number is divisible by 3, add it to the 'threes' array.
 let threes = []
-
+for (let i = 20; i < 100; i++) {
+  if (i % 3 == 0) {
+    threes.push(i)
+  }
+}
 
 
 // ------------------------------------------
@@ -23,16 +27,18 @@ let threes = []
 let scores = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98];
 
 
-let average = 0;
+// let average = 0;
 
 
 // code below
-debugger
-function findAverage(scores) {
-  let average = 0
-  let d = scores.length
-  for (let i = 0; i < scores.length; i++) {
-    average += scores[i]
-  }
-  return average / d
-}
+// function findAverage(scores) {
+//   let average = 0
+//   for (let i = 0; i < scores.length; i++) {
+//     sum += scores[i]
+//     average = sum / scores.length
+//   }
+//   return average
+// }
+
+let newSum = scores.reduce((previous, current) => current += previous);
+let average = newSum / scores.length
